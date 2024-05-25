aab12444df89033d81fba0ba9144a1b0f9a6d81f
BeheadedBadger <ZarradeRuijter@gmail.com>
2024-05-25 14:44:13 +0200

    main

---
+++ src/main/java/nl/novi/techiteasy_spring/TechItEasySpringApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasy_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechItEasySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/techiteasy_spring/TechItEasySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasy_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
